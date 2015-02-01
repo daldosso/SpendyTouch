cf1acb9f305837382abe8a20d20768ce5c60df05
Alberto Dal Dosso <daldosso@gmail.com>
2015-02-01 20:20:18 +0100

    Added menu

--- client/app/view/Main.js
+++ client/app/view/Main.js
@@ -12,10 +12,8 @@ Ext.define('SendyTouch.view.Main', {
             {
                 title: 'Expenses',
                 iconCls: 'bookmarks',
-
                 styleHtmlContent: true,
                 scrollable: true,
-
                 items: {
                     docked: 'top',
                     xtype: 'titlebar',
@@ -29,20 +27,40 @@ Ext.define('SendyTouch.view.Main', {
                 ].join("")
             },
             {
-                title: 'Get Started',
-                iconCls: 'action',
-
-                items: [
-                    {
+                title: 'Search',
+                iconCls: 'search',
+                items: [{
                         docked: 'top',
                         xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
+                        title: 'Search'
                     }
+                ]
+            }, {
+                title: 'Categories',
+                iconCls: 'compose',
+                items: [{
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Categories'
+                    }
+                ]
+            }, {
+                title: 'Stats',
+                iconCls: 'info',
+                items: [{
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'Stats'
+                    }
+                ]
+            }, {
+                title: 'Logout',
+                iconCls: 'action',
+                items: [{
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Stats'
+                }
                 ]
             }
         ]
